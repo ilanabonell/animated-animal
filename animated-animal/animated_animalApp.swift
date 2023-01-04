b9c56154d3ae49b029074ffd0739332e77dbe5c0
ilanabonell <ilana.bonell@gmail.com>
2023-01-04 10:41:00 -0500

    Initial Commit

---
+++ animated-animal/animated_animalApp.swift
@@ -0,0 +1,17 @@
+//
+//  animated_animalApp.swift
+//  animated-animal
+//
+//  Created by ilana on 1/4/23.
+//
+
+import SwiftUI
+
+@main
+struct animated_animalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
